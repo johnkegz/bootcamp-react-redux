44026ca2fb6db7de151eb754303f8c89e21cb79c
kalyango <johnkal24@gmail.com>
2019-01-01 16:09:55 +0300

    Revert "162899888 set up test environment "

--- tests/setupTests.js
+++
@@ -1,7 +0,0 @@
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-global.fetch = require("jest-fetch-mock");
-global.fetchMock = require("fetch-mock");
-
-Enzyme.configure({ adapter: new Adapter() });
