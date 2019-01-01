7845ca4fffc10ff5114a0c8ef2947a61ac1b6dcb
johnkegz <johnkal24@gmail.com>
2019-01-01 17:05:18 +0300

    [ch #162900310]: Set up heroku

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const mode = process.env.NODE_ENV || 'production';
+
+
+const config = {
+  mode,
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|jpg|otf)$/i,
+        use: ['file-loader'],
+      }
+    ],
+  },
+  devServer: {
+    https: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+   new HTMLWebpackPlugin({
+     template: './src/index.html',
+   }),
+  ],
+};
+
+module.exports = config;
