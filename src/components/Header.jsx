525ebc1d0bc98706702cd9bc61b4a0d5304fd3f8
johnkegz <johnkal24@gmail.com>
2019-01-02 08:14:15 +0300

    [ft #162899893]: users should be able to log out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import SignedInStatus from "./SignedInStatus";
 
 
 const Header = () => (
@@ -9,21 +10,10 @@ const Header = () => (
         <NavLink to="/home" className="navbar-brand">
           <strong>stackoverflowlite</strong>
         </NavLink>
-        <NavLink to="/postQuestion" className="navbar-brand">
-          <strong>post question</strong>
-        </NavLink>
+        
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li>
-              <NavLink to={"/login"} className={"navbar-brand"}>
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to={"/signup"} className={"navbar-brand"}>
-                signup
-              </NavLink>
-            </li>
+        <ul className="navbar-nav ml-auto">
+          <SignedInStatus />
           </ul>
         </div>
       </div>
